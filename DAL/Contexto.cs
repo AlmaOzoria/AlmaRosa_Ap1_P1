88bcb86d5c363609003da9a01bbc11d7434f2ae1
Alma Ozoria <heisylior@hotmail.com>
2024-09-23 21:03:54 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using AlmaRosa_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+
+namespace AlmaRosa_Ap1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registro> Registros { get; set; }
+}
---
+++ Models/Registro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlmaRosa_Ap1_P1.Models
+{
+    public class Registro
+    {
+        [Key]
+
+        public int RegistroId { get; set; }
+    }
+}
