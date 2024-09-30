c31f374975cc0e303c9c0aca0bfb39108fb81e29
Alma Ozoria <heisylior@hotmail.com>
2024-09-30 18:38:48 -0400

    Cree nuevo Models llamado Prestamo

---
+++ Models/Prestamo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlmaRosa_Ap1_P1.Models;
+
+public class Prestamo
+{
+    [Key]
+    [Required (ErrorMessage = "Debe de ser mayor que uno")]
+    public int PrestamoId { get; set; }
+
+    [Required (ErrorMessage = "Este Campo es obligatorio")]
+    public string Deudor { get; set; }
+
+    [Required (ErrorMessage = "Este Campo es obligatorio")]
+    public string Concepto { get; set; }
+
+    [Required (ErrorMessage = "Este Campo es obligatorio")]
+    public decimal Monto { get; set; }
+}
