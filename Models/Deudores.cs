671610ed0d2df344e28018c1c7e5a0c50358c2a5
Alma Ozoria <heisylior@hotmail.com>
2024-10-11 00:22:42 -0400

    Creando Modelo Deudores

---
+++ Models/Deudores.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlmaRosa_Ap1_P1.Models
+{
+    public class Deudores
+    {
+        [Key]
+        [Required(ErrorMessage = "Debe de ser mayor que uno")]
+        public int DeudorId { get; set; }
+
+        [Required(ErrorMessage = "Debe de ser mayor que uno")]
+        public string Nombres { get; set; }
+
+    }
+}
